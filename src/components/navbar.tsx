0538f63a8070f87e7bca07b7e6ceba883c02ec95
jdavesa <86001040+jdavesa@users.noreply.github.com>
2021-10-22 12:20:45 +0200

    Features: Projects Section

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,23 +1,35 @@
-import {useState} from 'react'
-import styled, { keyframes } from 'styled-components'
+import { useState } from "react";
+import styled, { keyframes } from "styled-components";
+import Scroll from "react-scroll";
+import {
+  Link,
+  Button,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller,
+} from "react-scroll";
 
 interface INv {
-    shadow: string;
+  shadow: string;
 }
 
 const NavbarContainer = styled.div<INv>`
-    position: fixed;
-    padding-top: 20px;
-    z-index: 2;
-    width: 100vw;
-    height: 70px;
-    background-color: white;
-    animation: ${props =>
-    (props.shadow === 'default' ?
-    null : props.shadow === 'shadow'?
-        toShadow : toNoShadow)
-    } .3s forwards;
-`
+  position: fixed;
+  padding-top: 20px;
+  z-index: 2;
+  width: 100vw;
+  height: 50px;
+  background-color: rgba(255, 255, 255);
+  animation: ${(props) =>
+      props.shadow === "default"
+        ? null
+        : props.shadow === "shadow"
+        ? toShadow
+        : toNoShadow}
+    0.3s forwards;
+`;
 const toShadow = keyframes`
     from{
         box-shadow: 0px 0px 0px rgba(0, 0, 0, 0.041);
@@ -25,7 +37,7 @@ const toShadow = keyframes`
     to{
         box-shadow: 1px 5px 20px rgba(0, 0, 0, 0.08);;
     }
-`
+`;
 
 const toNoShadow = keyframes`
     from{
@@ -34,36 +46,50 @@ const toNoShadow = keyframes`
     to{
         box-shadow: 0px 0px 0px rgba(0, 0, 0, 0.041);;
     }
-`
+`;
 
-function Navbar(){
-    const [shadow, setShadow] = useState<string>('default')
-    const [notFirstTime,setNotFirstTime] = useState<boolean>(false)
+function Navbar() {
+  const [shadow, setShadow] = useState<string>("default");
+  const [notFirstTime, setNotFirstTime] = useState<boolean>(false);
 
+  const toShadow = () => {
+    if (window.scrollY >= 100) {
+      setShadow("shadow");
+      setNotFirstTime(true);
+    } else if (notFirstTime && window.scrollY <= 100) {
+      setShadow("noShadow");
+    }
+  };
 
-        
-        const toShadow = () => {
-        if(window.scrollY >= 100){
-            setShadow('shadow')
-            setNotFirstTime(true)
-        } else if (notFirstTime && window.scrollY <= 100) {
-            setShadow('noShadow')
-        }}
-        
+  window.addEventListener("scroll", toShadow);
 
-        window.addEventListener('scroll', toShadow)
-   
-
-
-    
-
-    return(
-        <NavbarContainer shadow={shadow}>
-            <div className="navbar">
-                <h3>Jordi Davesa</h3>
-            </div>
-        </NavbarContainer>
-    )
+  return (
+    <NavbarContainer shadow={shadow}>
+      <div className="navbar">
+        <h3>Jordi Davesa</h3>
+        <div>
+          <Link
+            className="navbar-link"
+            to="projects"
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            Projects
+          </Link>
+          <Link
+            className="navbar-link"
+            to="projects"
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            Contact
+          </Link>
+        </div>
+      </div>
+    </NavbarContainer>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/home.js
+++ src/pages/home.js
@@ -3,10 +3,19 @@ import projectImage from "../Illustration4.png";
 import { useState, useEffect } from "react";
 import Fade from "react-reveal/Fade";
 import skills from "../Component-skills.svg";
-import tooly from "../tooly.png";
+import toolyPrj from "../tooly.png";
+import moverdPrj from "../moverd.png";
+import ccrPrj from "../ccr.png";
+import toolyLogo from "../tooly-logo.png";
+import moverdLogo from "../moverd-logo.png";
+import ccrLogo from "../ccr-logo.png";
+import { Link, Element } from "react-scroll";
 
 function HomePage() {
   const [offsetY, setOffsetY] = useState(0);
+  const [tooly, setTooly] = useState(false);
+  const [moverd, setMoverd] = useState(false);
+  const [ccr, setCcr] = useState(false);
 
   const handleScroll = () => setOffsetY(window.pageYOffset);
 
@@ -14,6 +23,23 @@ function HomePage() {
     window.addEventListener("scroll", handleScroll);
     return () => window.addEventLinstener("scroll", handleScroll);
   }, []);
+
+  const toggleTooly = () => {
+    setTooly(true);
+    setMoverd(false);
+    setCcr(false);
+  };
+
+  const toggleMoverd = () => {
+    setTooly(false);
+    setMoverd(true);
+    setCcr(false);
+  };
+  const toggleCcr = () => {
+    setTooly(false);
+    setMoverd(false);
+    setCcr(true);
+  };
 
   return (
     <div className="home-page">
@@ -42,17 +68,68 @@ function HomePage() {
           </Fade>
         </div>
       </div>
-      <div className="projects-container">
-        <div className="projects-title">
-          <Fade bottom>
-            <img src={projectImage} alt={projectImage} />
-            <h1>Selected projects</h1>
-          </Fade>
-          <div className="project-container">
-            <img src={tooly} alt={tooly} />
+      <Element name="projects">
+        <div className="projects-container">
+          <div className="projects-title">
+            <Fade bottom>
+              <h3>SOME</h3>
+              <h1>Selected projects</h1>
+              {/*    <img src={projectImage} alt={projectImage} /> */}
+            </Fade>
+          </div>
+          <div className="project-list">
+            <a onClick={toggleTooly} className="project-link">
+              <img src={toolyLogo} alt={toolyLogo} />
+            </a>
+            <a onClick={toggleMoverd} className="project-link">
+              <img src={moverdLogo} alt={moverdLogo} />
+            </a>
+            <a onClick={toggleCcr} className="project-link">
+              <img src={ccrLogo} alt={ccrLogo} />
+            </a>
+          </div>
+          <div style={{ transform: `translateY(${offsetY * 0.05}px)` }}>
+          {tooly && (
+            <Fade bottom>
+              <div className="project-container">
+                <img src={toolyPrj} alt={toolyPrj} />
+                <h3>
+                  Tooly is a tool sharing platform. The idea is to rent the
+                  tools we got at home creating community, sharing resources,
+                  evading the consumerism and reusing items.
+                </h3>
+              </div>
+            </Fade>
+          )}
+          {moverd && (
+            <Fade bottom>
+              <div className="project-container">
+                <img src={moverdPrj} alt={moverdPrj} />
+                <h3>
+                  Tooly is a tool sharing platform. The idea is to rent the
+                  tools we got at home creating community, sharing resources,
+                  evading the consumerism and reusing items.
+                </h3>
+              </div>
+            </Fade>
+          )}
+          <div>
+            {ccr && (
+              <Fade>
+                <div className="project-container">
+                  <img src={ccrPrj} alt={ccrPrj} />
+                  <h3>
+                    Tooly is a tool sharing platform. The idea is to rent the
+                    tools we got at home creating community, sharing resources,
+                    evading the consumerism and reusing items.
+                  </h3>
+                </div>
+              </Fade>
+            )}
+            </div>
           </div>
         </div>
-      </div>
+      </Element>
     </div>
   );
 }
